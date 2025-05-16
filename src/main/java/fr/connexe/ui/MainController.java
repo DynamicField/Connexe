b2a62adaf70fc358573da39882c854389b70f7f3
victor <victor.gavioli2004@gmail.com>
2025-05-16 15:17:09 +0200

    Merge Main

--- src/main/java/fr/connexe/ui/MainController.java
+++ src/main/java/fr/connexe/ui/MainController.java
@@ -2,7 +2,7 @@ package fr.connexe.ui;
 
 import fr.connexe.ConnexeApp;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -14,22 +14,30 @@ public class MainController {
     private ConnexeApp connexeApp;
     private MazeController mazeController;
 
+    @FXML
+    private MenuItem change;
+
+    @FXML
+    public void initialize() {
+        change.setDisable(true);
+    }
+
      /// Is called by the main application to give a reference back to itself.
-     /// @param connexeApp main application
+     /// @param connexeApp - main application
     public void setConnexeApp(ConnexeApp connexeApp) {
         this.connexeApp = connexeApp;
     }
 
      /// References the MazeController to call its building methods from the menu bar options (new, edit...)
-     /// @param mazeController the MazeController to use maze related methods from (building, etc...)
+     /// @param mazeController - the MazeController to use maze related methods from (building, etc...)
     public void setMazeController(MazeController mazeController){
         this.mazeController = mazeController;
     }
 
     /// Initialize config for a FileChooser
-    /// Only accepts a file of ".con" extension
+    /// Only accepts file of ".con" extension
     /// Opens by default the last directory a user saved into/opened
-    private FileChooser initFileChooser(){
+    private File initFileChooser(){
         // Create a new file chooser dialog popup and set the opened directory to the last visited one
         FileChooser fileChooser = new FileChooser();
         File lastDir = Settings.getLastVisitedDirectory();
@@ -39,7 +47,13 @@ public class MainController {
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
                 "Fichiers CON (*.con)", "*.con");
         fileChooser.getExtensionFilters().add(extFilter);
-        return fileChooser;
+
+        // Retrieve selected file to update the last opened directory (persists across sessions)
+        File selected = fileChooser.showOpenDialog(connexeApp.getStage());
+        if (selected != null) {
+            Settings.setLastVisitedDirectory(selected.getParentFile());
+        }
+        return selected;
     }
 
     /// Option to create a new maze.
@@ -51,14 +65,8 @@ public class MainController {
         MazeRenderer mazeRenderer = new MazeRenderer();
         boolean okClicked = connexeApp.showNewMazeDialog(mazeRenderer);
         if (okClicked) { // Maze is generated, now query the controller to display it on the view
-            mazeController.setMazeRenderer(mazeRenderer);
-
-            // Create the maze grid on the view (also displays the maze in the console)
+            change.setDisable(false);
             mazeController.createMazeFX();
-            System.out.println(mazeController.getMazeRenderer().getLog()); // show generation logs in console
-
-            connexeApp.updateStageTitle(null);
-            connexeApp.setMazeFilePath(null);
         }
     }
 
@@ -66,77 +74,42 @@ public class MainController {
     @FXML
     private void handleOpen() {
         // Init the FileChooser and retrieve the selected file
-        FileChooser fileChooser = initFileChooser();
-        fileChooser.setTitle("Ouvrir un labyrinthe");
-
-        // Retrieve selected file
-        File selected = fileChooser.showOpenDialog(connexeApp.getStage());
-
-        if (selected != null) {
-            // update the last opened directory (persists across sessions)
-            Settings.setLastVisitedDirectory(selected.getParentFile());
-            try{
-                // Initialize a renderer and give the loaded maze to the renderer to display on the view
-                MazeRenderer mazeRenderer = new MazeRenderer();
-                mazeController.setMazeRenderer(mazeRenderer);
-                mazeController.loadMaze(selected);
-
-                // Update current opened file path in the app
-                connexeApp.setMazeFilePath(selected);
-                connexeApp.updateStageTitle(selected.getName());
-            } catch(Exception e){
-                showError("Erreur lors du chargement du labyrinthe", e.getMessage());
-            }
+        File selectedFile = initFileChooser();
+        if(selectedFile != null) {
+            System.out.println("Open triggered...");
+            // To do : open maze from file
+            //connexeApp.loadMaze(file); or something like that
         }
     }
 
-    /// Saves the currently opened maze file
+    /// Saves the maze into a file
     /// If the current maze has never been saved before, the "save as" dialog is shown.
     @FXML
     private void handleSave() {
-        File mazeFile = connexeApp.getMazeFilePath();
-        if (mazeFile != null) { // If file has already been saved once, just save over it
-            try{
-                mazeController.saveMaze(mazeFile);
-            } catch(Exception e){
-                showError("Erreur lors de la sauvegarde du labyrinthe", e.getMessage());
-            }
+        System.out.println("Save triggered...");
+        /*File mazeFile = connexeApp.getMazeFilePath();
+        if (mazeFile != null) { // If file has already been saved once, just save above
+            // To do : save maze data to export as a file
+            //connexeApp.saveMaze(file); or something like that
         } else { // else, act as "Save as..." option
             handleSaveAs();
+        }*/
+    }
+
+    /// Opens a FileChooser to let the user select a file to save to.
+    @FXML
+    private void handleSaveAs() {
+        // Init the FileChooser and retrieve the selected file
+        File selectedFile = initFileChooser();
+        if (selectedFile != null) {
+            System.out.println("Save as triggered...");
+            // To do : save maze data to export as a file
+            //connexeApp.saveMaze(file); or something like that
         }
     }
 
-    /// Opens a FileChooser to let the user save a maze into a .con file
-    @FXML
-    private void handleSaveAs() {
-        if(mazeController.getMazeRenderer() != null){
-            // Init the FileChooser and retrieve the file
-            FileChooser fileChooser = initFileChooser();
-            fileChooser.setTitle("Enregistrer le labyrinthe");
+    public void handleChange(){
 
-            // Show save file dialog on top of stage
-            File mazeFile = fileChooser.showSaveDialog(connexeApp.getStage());
-
-            if (mazeFile != null) {
-                // Make sure it has the correct extension
-                if (!mazeFile.getPath().endsWith(".con")) {
-                    mazeFile = new File(mazeFile.getPath() + ".con");
-                }
-                Settings.setLastVisitedDirectory(mazeFile.getParentFile());
-                try{
-                    // Save the maze to the selected file
-                    mazeController.saveMaze(mazeFile);
-
-                    // Update the app title with the current saved file
-                    connexeApp.setMazeFilePath(mazeFile);
-                    connexeApp.updateStageTitle(mazeFile.getName());
-                } catch (Exception e) {
-                    showError("Erreur lors de la sauvegarde du labyrinthe", e.getMessage());
-                }
-            }
-        } else {
-            showError("Aucun labyrinthe créé", "Veuillez créer un labyrinthe avant de le sauvegarder.");
-        }
     }
 
     ///  Closes the app
@@ -148,26 +121,11 @@ public class MainController {
     ///  Menu Item option to show a hardcoded example maze
     @FXML
     private void handleExampleMaze(){
+        System.out.println("Testing default maze...");
+
         // Initialize a renderer taking a maze generated from user parameters through the creation dialog box
         MazeRenderer mazeRenderer = new MazeRenderer();
         mazeRenderer.setDefaultExample();
-        mazeController.setMazeRenderer(mazeRenderer);
         mazeController.createMazeFX();
-
-        // Update the app title
-        connexeApp.updateStageTitle("Exemple");
-        connexeApp.setMazeFilePath(null);
-    }
-
-    /// Show an error dialog box
-    /// @param error the title displayed in the dialog box
-    /// @param details the content of the error message
-    private void showError(String error, String details){
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Erreur");
-        alert.setHeaderText(error);
-        alert.setContentText(details);
-
-        alert.showAndWait();
     }
 }
